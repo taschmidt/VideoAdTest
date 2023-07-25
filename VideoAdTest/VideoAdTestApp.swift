44e3d5a47bf270a0f75e2c80ae1e3d3d81399672
Tim Schmidt <tim.schmidt@dowjones.com>
2023-07-25 10:19:17 -0500

    Initial Commit

---
+++ VideoAdTest/VideoAdTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  VideoAdTestApp.swift
+//  VideoAdTest
+//
+//  Created by schmidtt on 7/25/23.
+//
+
+import SwiftUI
+
+@main
+struct VideoAdTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
